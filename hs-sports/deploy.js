cf16a7351a96327c6880134b98baf0de7af8ffb8
techmsi <sophia.idro@gmail.com>
2017-09-08 11:44:41 -0400

    Add releases

---
+++ hs-sports/deploy.js
@@ -0,0 +1,28 @@
+// 1) Bump version
+// 2) Get config
+const { name:PROJECT_NAME, version, repository: { url : repo } } = require('./package.json');
+const ghpages = require('gh-pages');
+const timestamp = require('console-timestamp');
+var path = require('path');
+
+const isDev = process.argv.indexOf('--dev') !== -1;
+const DIST_DIR = path.join(__dirname, 'public', 'dist');
+const now = new Date();
+
+const options = {
+  branch: 'master',
+  dest: `${PROJECT_NAME}/public/dist`,
+  add: true,
+  tag: `v.hss.${version}`,
+  src: isDev ? '**/dev.*' : '**/app.*',
+  message: `${PROJECT_NAME} ${version}: ${isDev ? 'Dev' : 'Prod'} - ${timestamp('Automated Build on MM/DD/YYYY at hh:mm', now)}`
+};
+
+// 3) Deploy to remote
+ghpages.publish(DIST_DIR, options, (err) => {
+  if (err) {
+    console.log('err ', err);
+  } else {
+    console.log(`Pubilshed ${options.message}, successfully.\n${JSON.stringify(options, null, 2)}`);
+  }
+});
---
+++ hs-sports/public/dist/js/dev.app.js
@@ -0,0 +1,1 @@
+Automated Build on 09/13/2017 at 16:14
