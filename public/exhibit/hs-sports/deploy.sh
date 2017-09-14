fa14c17668366e901380789836106f0f821d80b0
techmsi <sophia.idro@gmail.com>
2017-09-14 13:26:46 -0400

    fix deploy sh

--- public/exhibit/hs-sports/deploy.sh
+++ public/exhibit/hs-sports/deploy.sh
@@ -9,8 +9,9 @@ git checkout $branchName;
 # git merge master --no-edit # Merge in the master branch without prompting
 npm run dev:build # Generate the bundled Javascript and CSS
 if $(git commit -am $branchName); then # Commit the changes, if any
-  echo 'Changes Committed for' + $branchName;
+  echo "Committed changes for $branchName";
 fi
-git push origin/$branchName # Deploy to Github
+# git push origin/$branchName # Deploy to Github
+echo "Pushed changes to $branchName";
 git checkout master # Checkout master again
 git stash pop # And restore the changes
