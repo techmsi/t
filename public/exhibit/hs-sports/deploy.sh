9487e63d771c6c7b3b55595745a8835126722db1
techmsi <sophia.idro@gmail.com>
2017-09-14 14:11:35 -0400

    fix sh

--- public/exhibit/hs-sports/deploy.sh
+++ public/exhibit/hs-sports/deploy.sh
@@ -3,24 +3,27 @@ set -o errexit # Exit on error
 
 timestamp=$(date +'%s');
 branchName="deploy-$timestamp";
+
 echo "Beginning automated deployment on $branchName";
 
-git stash save 'Before deploy' # Stash all changes before deploy
+# Stash all changes before deploy
+git stash save 'Before deploy';
 
-git checkout $branchName;
+git checkout -b $branchName;
 echo "Checkout: $branchName";
-# git merge master --no-edit # Merge in the master branch without prompting
+# Merge in the master branch without prompting
+# git merge master --no-edit
 
 # Generate the bundled Javascript and CSS
 npm run dev:build
 # Commit the changes, if any
-if $(git commit -am $branchName); then 
+if $(git commit -am "Automated deploy"); then 
   echo "Commit: Changes for $branchName";
 fi
 
 # Deploy to Github
-git push origin/$branchName 
 echo "Push: Changes to $branchName";
+git push origin $branchName
 
 # Checkout master again
 git checkout master
