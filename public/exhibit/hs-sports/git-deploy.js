931965dca6b52d0d0e981baddedd02abe3434782
techmsi <sophia.idro@gmail.com>
2017-09-14 14:49:59 -0400

    Add push

--- public/exhibit/hs-sports/git-deploy.js
+++ public/exhibit/hs-sports/git-deploy.js
@@ -10,8 +10,14 @@ const branchName = `build_${timestamp('MM-DD-YYYY_hh-mm-ss', now)}`;
 const message = `${PROJECT_NAME} ${version}: ${isDev ? 'Dev' : 'Prod'} - ${timestamp('Automated Build on MM/DD/YYYY at hh:mm', now)}`;
 
 git()
-.checkoutBranch(branchName, 'origin/master', (err, ok) => {
- console.log("ok ", ok);
+.checkoutBranch(branchName, 'origin/master', (err) => {
+ console.log("Checked out ", branchName);
 })
 .add('./*')
-.commit(message);
+.commit(message)
+.push(['origin', branchName], function () {
+ console.log("Pushed ", branchName);
+})
+// .checkout('origin/master', function () {
+//   console.log("Checked out origin/master");
+// });
